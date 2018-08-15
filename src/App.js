e39c291545baca1a50d10bf65865842395b3daea
Joshe <femijay9@gmail.com>
2018-08-15 18:19:42 +0200

    added pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './Pages/home/Home'
+import About from './Pages/about/About';
+import Movies from './Pages/movies/Movies';
+import FAQ from './Pages/faq/FAQ';
+import Celebs from './Pages/celebs/Celebs';
+import Error from './Pages/error/Error';
+import Terms_and_Conditions from './Pages/terms/tandc';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">React Login Page</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+				<Switch>
+					<Route path="/" component={Home} exact/>
+					<Route path="/about" component={About} />
+					<Route path="/movies" component={Movies} />
+					<Route path="/faq" component={FAQ} />
+					<Route path="/celebs" component={Celebs} />
+					<Route path="/terms" component={Terms_and_Conditions} />
+					<Route path="*" component={Error} />
+				</Switch>
+			</BrowserRouter>
     );
   }
 }
 
 export default App;
-
-// npm run start
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'font-awesome/css/font-awesome.css' 
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
