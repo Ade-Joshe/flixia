14440076212f0f6e3cd64636d8e662e207006607
Ade-Joshe <femijay9@gmail.com>
2018-08-17 22:56:59 +0100

    homepage is ready for deploy

--- src/Pages/home/Home.js
+++ src/Pages/home/Home.js
@@ -3,26 +3,33 @@ import Header from '../components/header';
 import Footer from '../components/footer';
 import './home.css';
 import '../general_style.css'
-import MovieTab from '../components/movTab.js'
-import LoginModal from '../components/loginModal'
-
+import MovieTab from '../components/movTab.js';
+import Slide from '../components/slideshow';
+// import img from '../images/img_parallax.jpg'
 
 const Home = () => {
     return (
-        <div>
+        <div style={{height: '100%'}}>
             {/* <div className="header"> */}
             {/* //header goes here */}
                 <Header />
             {/* </div> */}
-            <div className="jumbotron general">
+            <div className="bighead general">
                  {/* the slider goes here */}
-                
+                {/* <span> <img src={img} alt='' className='imag'/></span> */}
+                <Slide />
             </div>
-            <MovieTab />
+            <div style={{position: "relative", marginTop: -50}}>
+                <MovieTab />
+            </div>
             {/* movie cards grid comes here */}
+
+            <div className='myloader'>
+                <button className='btn btn-md but'>Load More &nbsp;&nbsp; <i className='fa fa-refresh'></i></button>
             <hr />
-            {/* page footer goes here */}
                 <Footer /> 
+            </div>
+            {/* page footer goes here */}
         </div>
     )
 }
