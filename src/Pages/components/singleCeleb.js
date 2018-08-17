9fe72d04f6c4472f4bc6f545cc9c7f9ddc3f0490
Ade-Joshe <femijay9@gmail.com>
2018-08-18 00:37:34 +0100

    heroku setup done

---
+++ src/Pages/components/singleCeleb.js
@@ -0,0 +1,14 @@
+ import React from 'react'
+import Header from './header';
+import Footer from './footer';
+import './singleCeleb.css'
+
+const SingleCelebrity = () => {
+    return(<div>
+        <Header/>
+            <div className='big-celeb-body'>
+
+            </div>
+        <Footer/>
+    </div>)
+}
