cb20a0e18ebcfb8891ae7045ab507b0e865f84a0
Ade-Joshe <femijay9@gmail.com>
2018-08-17 22:56:38 +0100

    npm installed jquery

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'font-awesome/css/font-awesome.css' 
 import 'semantic-ui-css/semantic.min.css';
+import 'jquery/dist/jquery'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
