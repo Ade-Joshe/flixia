54a15477e0dbc8e6b2592cff0d42c3d5076cd042
Ade-Joshe <femijay9@gmail.com>
2018-08-17 22:58:28 +0100

    made my search component reusable.... so sweet

--- src/Pages/components/search.js
+++ src/Pages/components/search.js
@@ -2,16 +2,19 @@ import React, { Component } from 'react';
 // const TMDBLogo = 'https://www.themoviedb.org/assets/static_cache/27b65cb40d26f78354a4ac5abf87b2be/images/v4/logos/powered-by-rectangle-green.svg';
 
 class SearchBox extends Component {
+  constructor(props){
+    super(props);
 
+  }
   handleChange(event) {
     event.target.select();
   }
   render() {
     return (
-      <div className="input-group mb-3" style={{width:  '40%', float: 'right', paddingRight: '7px'}}>
-        <input type="text" className="form-control" ref="search suggestion" onClick={this.handleChange} placeholder="Search Movie Title.." aria-label="Recipient's username" aria-describedby="basic-addon2" />
-        <div className="input-group-append">
-          <span className="input-group-text fa fa-search" id="basic-addon2" style={{marginTop: 0, paddingTop: 9}}></span>
+      <div className="input-group mb-3" style={{width: this.props.width, float: this.props.float, paddingRight: '7px'}}>
+        <input type="text" className="form-control" ref="search suggestion" onClick={this.handleChange} placeholder={this.props.placeholder} aria-label="Recipient's username" aria-describedby="basic-addon2" />
+        <div className="input-group-append" >
+          <span className={"input-group-text fa fa-"+ this.props.icon} id="basic-addon2" style={{marginTop: 0, paddingTop: 9, backgroundColor: 'gold'}}></span>
         </div>
       </div>
     )
