a6fc80ffec3386c29e3672e3062b7c4829e2b91a
Ade-Joshe <femijay9@gmail.com>
2018-08-23 03:53:54 +0100

    modal responsiveness fixed

--- src/Pages/components/loginModal.js
+++ src/Pages/components/loginModal.js
@@ -1,55 +1,66 @@
 import React, { Component } from 'react'
-import { Button, Modal } from 'semantic-ui-react'
 import './style.css'
+import Modal from "react-responsive-modal";
 import { Icon, Input } from 'semantic-ui-react';
 
-class LoginModal extends Component {
-  state = { open: false }
 
-  show = dimmer => () => this.setState({ dimmer, open: true })
-  close = () => this.setState({ open: false })
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+
+class LoginModal extends Component {
+		state = {
+    open: false
+  };
+
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
 
   render() {
-    const { open, dimmer } = this.state
-
+    const { open } = this.state;
     return (
-      <span>
-        <p onClick={this.show('blurring')} className="trigger smoke" >Login</p>
-          <div>
-			<Modal dimmer={dimmer} open={open} onClose={this.close} style={{margin: 'auto', height: 520, marginTop: 'auto', paddingLeft: '2%', paddingRight: '2%', width: 530 }}>
-				<div className="modal-body">
-					<h1 onClick={this.close} className="modalClose fa fa-close"></h1>
-
-						<div className="modal-content">
-						<button className="btn btn-md btn-primary col-span-12 mass"> <i className="fa fa-facebook"></i> Login with facebook </button>
-							<button className="btn btn-md btn-default col-span-12 mass"> <i className="fa fa-google"></i> Login with Google </button>
-							<span style={{margin: '3%'}}> or </span>
-						<form style={{display: 'contents'}}onSubmit={this.handleSubmit}>
-							<span style={{marginBottomt: '20px'}}><Input iconPosition='left' placeholder='Username or Email' className="myInput">
-								<Icon name='user' />
-								<input />
-							</Input> 
-							</span>
-							<span><br />
-							<Input iconPosition='left' placeholder='Password'  className="myInput">
-								<Icon name='eye' />
-								<input type="password"/>
-							</Input>
-							</span>
-							<span className="modal-deet">
-								<span style={{paddingLeft: '15%', float: 'left'}}>
-									<input type="checkbox" /> Remember Me </span>
-								<span style={{paddingRight: '15%', float: 'right'}}> Forgot password </span>
-							</span>
-						<button className="btn btn-md btn-primary col-span-12 mass"> Login with </button>
-						</form>
-					</div>
-				</div>
-			</Modal>
-		  </div>
-		</span>
-    )
+      <div style={styles}>
+        <p onClick={this.onOpenModal} className='trigger smoke'>Login</p>
+        <Modal open={open} onClose={this.onCloseModal} center>
+				<div className="modal-body">			
+					 <div className="modal-content">
+					 <button className="btn btn-md btn-primary col-span-12 mass"> <i className="fa fa-facebook"></i> Login with facebook </button>
+						 <button className="btn btn-md btn-default col-span-12 mass"> <i className="fa fa-google"></i> Login with Google </button>
+						 <span style={{margin: '3%'}}> or </span>
+					 <form style={{display: 'contents'}}onSubmit={this.handleSubmit}>
+						 <span style={{marginBottomt: '20px'}}><Input iconPosition='left' placeholder='Username or Email' className="myInput">
+							 <Icon name='user' />
+							 <input />
+						 </Input> 
+						 </span>
+						 <span><br />
+						 <Input iconPosition='left' placeholder='Password'  className="myInput">
+							 <Icon name='eye' />
+							 <input type="password"/>
+						 </Input>
+						 </span>
+						 <span className="modal-deet">
+							 <span style={{float: 'left'}}>
+								 <input type="checkbox" /> Remember Me </span>
+							 <span style={{float: 'right'}}> Forgot password </span>
+						 </span>
+					 <button className="btn btn-md btn-primary col-span-12 mass"> Login </button>
+					 <p> </p>
+					 </form>
+				 </div>
+			 </div> 
+        </Modal>
+      </div>
+    );
   }
+
 }
 
 export default LoginModal
--- src/Pages/components/signupModal.js
+++ src/Pages/components/signupModal.js
@@ -1,66 +1,92 @@
 import React, { Component } from 'react'
-import { Modal } from 'semantic-ui-react'
 import './style.css'
+import Modal from "react-responsive-modal";
 import { Icon, Input } from 'semantic-ui-react';
 
 
+const styles = {
+  fontFamily: "sans-serif",
+  textAlign: "center"
+};
+
+
 class SignUpModal extends Component {
-  state = { open: false }
+	state = {
+		open: false
+  };
 
-  show = dimmer => () => this.setState({ dimmer, open: true })
-  close = () => this.setState({ open: false })
-
+  onOpenModal = () => {
+    this.setState({ open: true });
+  };
+	
+  onCloseModal = () => {
+    this.setState({ open: false });
+  };
+	
   render() {
-    const { open, dimmer } = this.state;
-
+    const { open } = this.state;
     return (
-      <span>
-        <p onClick={this.show('blurring')} className='trigger smoke' style={{marginRight: 10}}>SignUp</p>
-          <div>
-			<Modal dimmer={dimmer} open={open} onClose={this.close} style={{margin: 'auto', height: 520, marginTop: 'auto', paddingLeft: '2%', paddingRight: '2%', width: 530 }}>
-				<div className="modal-body">
-					<h1 onClick={this.close} className="modalClose fa fa-close"></h1>
-
-						<div className="modal-content" style={{paddingTop: '5%'}}>
-						<button className="btn btn-md btn-primary col-span-12 mass"> <i className="fa fa-facebook"></i> SignUp with facebook </button>
-							<button className="btn btn-md btn-default col-span-12 mass"> <i className="fa fa-google"></i> SignUp with Google </button>
-							{/* <hr /> */}
-							<span style={{margin: '3%'}}> or </span>
-
-						<form style={{display: 'contents'}}onSubmit={this.handleSubmit}>
-						<span style={{marginBottom: '20px'}}>
-							<Input iconPosition='left' placeholder='Enter Name' className="myInput">
-								<Icon name='user' />
-								<input />
-							</Input> 
-							</span>							
-							<span>
-							<Input iconPosition='left' placeholder='Email' className="myInput">
-								<Icon name='at' />
-								<input />
-							</Input> 
-							</span>
-							<span><br />
-							<Input iconPosition='left' placeholder='Password'  className="myInput">
-								<Icon name='eye' />
-								<input type="password"/>
-							</Input>
-							</span>
-							<span className="modal-deet">
-								<span style={{ float: 'left'}}>
-									<input type="checkbox" /> Remember Me </span>
-								<span style={{float: 'right'}}> Forgot password ? </span>
-							</span>
-						<button className="btn btn-md btn-primary col-span-12 mass"> SignUp </button>
-						</form>
-						<span style={{margin: 'auto', marginTop: '1%'}}>Already have an account </span>
-				</div>
-				</div>
-			</Modal>
-		  </div>
-		</span>
-    )
-  }
+      <div style={styles}>
+				<p onClick={this.onOpenModal} className='trigger smoke'>SignUp</p>
+				<Modal open={open} onClose={this.onCloseModal} center>
+					<div className="modal-body">		
+							<div className="modal-content" style={{paddingTop: '5%'}}>
+							<button className="btn btn-md btn-primary col-span-12 mass"> <i className="fa fa-facebook"></i> SignUp with facebook </button>
+								<button className="btn btn-md btn-default col-span-12 mass"> <i className="fa fa-google"></i> SignUp with Google </button>
+								{/* <hr /> */}
+								<span style={{margin: '3%'}}> or </span>
+				
+							<form style={{display: 'contents'}}onSubmit={this.handleSubmit}>
+							<span style={{marginBottom: '20px'}}>
+								<Input iconPosition='left' placeholder='Enter Name' className="myInput">
+									<Icon name='user' />
+									<input />
+								</Input> 
+								</span>							
+								<span>
+								<Input iconPosition='left' placeholder='Email' className="myInput">
+									<Icon name='at' />
+									<input />
+								</Input> 
+								</span>
+								<span><br />
+								<Input iconPosition='left' placeholder='Password'  className="myInput">
+									<Icon name='eye' />
+									<input type="password"/>
+								</Input>
+								</span>
+								<span className="modal-deet">
+									<span style={{ float: 'left'}}>
+										<input type="checkbox" /> Remember Me </span>
+									<span style={{float: 'right'}}> Forgot password ? </span>
+								</span>
+							<button className="btn btn-md btn-primary mass"> SignUp </button>
+							</form>
+							<span style={{margin: 'auto', marginTop: '1%'}}>Already have an account </span>
+					</div>
+					</div>
+				</Modal>
+			</div>
+			)
+	}
 }
 
 export default SignUpModal;
+// state = { open: false }
+
+// show = dimmer => () => this.setState({ dimmer, open: true })
+// close = () => this.setState({ open: false })
+
+// render() {
+//   const { open, dimmer } = this.state;
+
+//   return (
+//     <span>
+//       <p onClick={this.show('blurring')} className='trigger smoke' style={{marginRight: 10}}>SignUp</p>
+//         <div>
+// 		<Modal dimmer={dimmer} open={open} onClose={this.close} style={{margin: 'auto', height: 520, marginTop: 'auto', paddingLeft: '2%', paddingRight: '2%', width: 530 }}>
+// 		</Modal>
+// 	  </div>
+// 	</span>
+//   )
+// }
