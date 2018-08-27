563476ee2939c94af85285050655ff78c88db637
Ade-Joshe <femijay9@gmail.com>
2018-08-27 05:56:57 +0100

    login and redirect with dummy api tested

--- src/Pages/components/loginModal.js
+++ src/Pages/components/loginModal.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import './style.css'
 import Modal from "react-responsive-modal";
 import { Icon, Input } from 'semantic-ui-react';
-
+import axios from  'axios'
+import API from '../components/api'
 
 const styles = {
   fontFamily: "sans-serif",
@@ -11,48 +12,72 @@ const styles = {
 
 
 class LoginModal extends Component {
-		state = {
-    open: false
-  };
+	state = {
+		open: false,
+		email: '',
+		password: ''
+	};
 
-  onOpenModal = () => {
-    this.setState({ open: true });
-  };
+	onOpenModal = () => {
+		this.setState({ open: true });
+		this.props.toggle('login');
+	};
 
-  onCloseModal = () => {
-    this.setState({ open: false });
-  };
+	onCloseModal = () => {
+		this.setState({ open: false });
+	};
+	
+	handleChange = event => {
+		this.setState({ 
+			[event.target.name]: event.target.value
+		});
+	}
 
-  render() {
-    const { open } = this.state;
+	handleSubmit = event => {
+		// event.preventDefault();
+		let { email, password } = this.state;
+	  
+		const user = {  email, password };
+		console.log(user);
+		// this.props.history.push('/singlemovie');
+		// API.post(`/user/${user_id}`, { user })
+		//   .then(res => {
+			//     console.log(res);
+			//     alert(res.data.message);
+			//   })
+		}
+
+	render() {
+		const { open } = this.state;
     return (
-      <div style={styles}>
-        <p onClick={this.onOpenModal} className='trigger smoke'>Login</p>
-        <Modal open={open} onClose={this.onCloseModal} center>
+		<div style={styles}>
+			<p onClick={this.onOpenModal} className='trigger smoke'>Login</p>
+			<Modal open={open} onClose={this.onCloseModal} center>
 				<div className="modal-body">			
-					 <div className="modal-content">
-					 <button className="btn btn-md btn-primary col-span-12 mass"> <i className="fa fa-facebook"></i> Login with facebook </button>
+					<div className="modal-content" onClick={this.isOpen}>
+						<button className="btn btn-md btn-primary col-span-12 mass"> <i className="fa fa-facebook"></i> Login with facebook </button>
 						 <button className="btn btn-md btn-default col-span-12 mass"> <i className="fa fa-google"></i> Login with Google </button>
 						 <span style={{margin: '3%'}}> or </span>
-					 <form style={{display: 'contents'}}onSubmit={this.handleSubmit}>
-						 <span style={{marginBottomt: '20px'}}><Input iconPosition='left' placeholder='Username or Email' className="myInput">
-							 <Icon name='user' />
-							 <input />
-						 </Input> 
+							<form style={{display: 'contents'}} onSubmit={this.handleSubmit}>
+								<span style={{marginBottomt: '20px'}}>
+								<Input iconPosition='left' placeholder='you@example.com' className="myInput">
+									<Icon name='at' />
+									<input name='email' onChange={this.handleChange} value={this.state.email}/>
+								</Input> 
 						 </span>
 						 <span><br />
 						 <Input iconPosition='left' placeholder='Password'  className="myInput">
 							 <Icon name='eye' />
-							 <input type="password"/>
+							 <input type="password" name='password' onChange={this.handleChange} value={this.state.password}/>
 						 </Input>
 						 </span>
 						 <span className="modal-deet">
 							 <span style={{float: 'left'}}>
-								 <input type="checkbox" /> Remember Me </span>
+								 <input type="checkbox" onChange={this.handleChange} /> Remember Me </span>
 							 <span style={{float: 'right'}}> Forgot password </span>
 						 </span>
-					 <button className="btn btn-md btn-primary col-span-12 mass"> Login </button>
-					 <p> </p>
+						<button className="btn btn-md btn-primary col-span-12 mass"> Login </button>
+						<p> </p>
 					 </form>
 				 </div>
 			 </div> 
