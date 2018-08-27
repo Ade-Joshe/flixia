084adf000e75050810aa7cbfd4175f3a2bc10544
Ade-Joshe <femijay9@gmail.com>
2018-08-27 05:48:59 +0100

    added nested routing rules

--- src/App.js
+++ src/App.js
@@ -8,23 +8,24 @@ import Celebs from './Pages/celebs/Celebs';
 import Error from './Pages/error/Error';
 import Terms_and_Conditions from './Pages/terms/tandc';
 import SingleCeleb from './Pages/singleceleb/singleCeleb.js'
-
+import SingleMovie from './Pages/singlemovie/SingleMovie'
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-				<Switch>
-					<Route path="/" component={Home} exact/>
-					<Route path="/about" component={About} />
-					<Route path="/movies" component={Movies} />
-					<Route path="/faq" component={FAQ} />
-					<Route path="/celebs" component={Celebs} />
-					<Route path="/terms" component={Terms_and_Conditions} />
-					<Route path="/single_celebs" component={SingleCeleb} />
-					<Route path="*" component={Error} />
-				</Switch>
-			</BrowserRouter>
+		<BrowserRouter>
+			<Switch>
+				<Route path="/movies/singlemovie" component={SingleMovie} />
+				<Route path="/about" component={About} />
+				<Route path="/faq" component={FAQ} />
+				<Route path="/celebs" component={Celebs} />
+				<Route path="/terms" component={Terms_and_Conditions} />
+				<Route path="/single_celebs" component={SingleCeleb} />
+				<Route path="/movies" component={Movies} />
+				<Route path="/" component={Home} exact/>
+				<Route path="*" component={Error} />
+			</Switch>
+		</BrowserRouter>
     );
   }
 }
