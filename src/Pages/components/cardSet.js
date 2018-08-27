9defccf418b94ba9e2291a9b471385db719abbdf
Ade-Joshe <femijay9@gmail.com>
2018-08-27 05:54:28 +0100

    fixed layout on screen

--- src/Pages/components/cardSet.js
+++ src/Pages/components/cardSet.js
@@ -6,14 +6,16 @@ import Paper from '@material-ui/core/Paper';
 import './cardSet.css';
 // import josh from '../images/josh.JPG ';
 import image from '../images/avatar.jpg'
+import {NavLink} from 'reactstrap'
+
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    height: 330,
-    width: 260,
+    height: 315,
+    width: 243,
     padding: 10,
   },
   control: {
@@ -49,88 +51,110 @@ const styles = theme => ({
 
 
 class GuttersGrid extends React.Component {
-  state = {
-    spacing: '16',
-  };
+	state = {
+		spacing: '40',
+		// limit: 6
+	};
 
-  handleChange = key => (event, value) => {
-    this.setState({
-      [key]: value,
-    });
-  };
+	
+	// onLoadMore= () => {
+	// 	this.setState({
+	// 		limit: this.state.limit+6
+	// 	})
+	// }
 
-  render() {
-    const { classes } = this.props;
-    const { spacing } = this.state;
-    const movies = [
-      {
-        movie_title: 'Dumebi the dirty girl',
-        rating: 5,
-        comments: 40
-      },
-      {
-        movie_title: 'Aki and pawpaw',
-        rating: 78,
-        comments: 40
-      },   {
-        movie_title: 'Wedding Party',
-        rating: 4,
-        comments: 40
-      },   {
-        movie_title: 'Wedding Party2',
-        rating: 0,
-        comments: 40
-      },   {
-        movie_title: 'Alpha woolf of anambra',
-        rating: 55,
-        comments: 40
-      },   {
-        movie_title: 'Ikenga thieves',
-        rating: 35,
-        comments: 40
-      },   {
-        movie_title: 'Affiamuta in Nigeria',
-        rating: 50,
-        comments: 40
-      },
-      {
-        movie_title: 'Ndi ibo, ogeneTv na biago',
-        rating: 98,
-        comments: 40
-      },   {
-        movie_title: "Aka n'so by Dabs",
-        rating: 59,
-        comments: 40
-      },   {
-        movie_title: 'SweetSuzie is bae',
-        rating: 67,
-        comments: 40
-      },
-    ]
+	// renderTodos = () => {
+		// return movies.slice(0, this.state.limit).map((item))
+	// }
+
+	handleChange = key => (event, value) => {
+		this.setState({
+			[key]: value,
+		});
+	};
+
+	render() {
+		const { classes } = this.props;
+		const { spacing } = this.state;
+		const movies = [
+			{
+				movie_title: 'Dumebi the dirty girl',
+				rating: 5,
+				comments: 40
+			},
+			{
+				movie_title: 'Aki and pawpaw',
+				rating: 78,
+				comments: 40
+			},   {
+				movie_title: 'Wedding Party',
+				rating: 4,
+				comments: 40
+			},   {
+				movie_title: 'Wedding Party2',
+				rating: 0,
+				comments: 40
+			},   {
+				movie_title: 'Alpha woolf of anambra',
+				rating: 55,
+				comments: 40
+			},   {
+				movie_title: 'Ikenga thieves',
+				rating: 35,
+				comments: 40
+			},  {
+				movie_title: 'Affiamuta in Nigeria',
+				rating: 50,
+				comments: 40
+			},
+			{
+				movie_title: 'Ndi ibo, ogeneTv na biago',
+				rating: 98,
+				comments: 40
+			},   
+			{
+				movie_title: "Aka n'so by Dabs",
+				rating: 59,
+				comments: 40
+			},   
+			{
+				movie_title: 'SweetSuzie is bae',
+				rating: 67,
+				comments: 40
+			},
+			];
+	
     return (
-      <Grid container className={classes.root} spacing={16}>
-        <Grid item xs={12}>
-          <Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
-            {movies.map(value => (
-              <Grid key={value} item>
-                <Paper className={classes.paper}>
-                  <div className={classes.paperImage}>
-                  <img src={image} className={classes.image} alt=""/>
-                  </div>
-                  <div>
-                      <span style={{fontWeight: 'bolder'}}>{value.movie_title}</span> <br />
-                      <span><h3 className="fa fa-star icon"></h3> <span className="" style={{fontSize: '1rem', fontWeight: 'bolder'}}>{value.rating}</span>
-                       &nbsp;&nbsp;&nbsp;&nbsp;<b className="fa fa-comments icon"></b> <span  style={{fontSize: '1rem', fontWeight: 'bolder'}}>{value.comments}</span> </span>
-                  </div>
-                </Paper>
-                <h1 className="fa fa-loading "></h1>
-              </Grid>
-            ))}
-          </Grid>
-        </Grid>
-      </Grid>
-    );
-  }
+		<Grid container className={classes.root} spacing={40}>
+			<Grid item xs={12}>
+				<Grid container className={classes.demo} justify="center" spacing={Number(spacing)}>
+					{movies.map(value => (
+						<Grid key={value} item>
+							<Paper className={classes.paper}>
+								<div className={classes.paperImage}>
+									<a href={this.props.paperLink}>
+										<img src={image} className={classes.image} alt="" />
+									</a>
+								</div>
+								<div>
+									<span style={{fontWeight: 'bolder'}}>{value.movie_title}</span> <br />
+									<span>
+										<h3 className="fa fa-star icon"></h3> 
+										<span className="" style={{fontSize: '1rem', fontWeight: 'bolder'}}>{value.rating}</span>&nbsp;&nbsp;&nbsp;&nbsp;
+										<b className="fa fa-comments icon"></b> <span  style={{fontSize: '1rem', fontWeight: 'bolder'}}>{value.comments}</span> 
+									</span>
+								</div>
+							</Paper>
+						</Grid>
+					))}
+				</Grid>
+					<div className='myloader'>
+						<button className='btn btn-md but'>Load More &nbsp;&nbsp; <i className='fa fa-refresh'></i></button>
+					</div>
+			</Grid>
+		</Grid>
+	);
+}
 }
 
 GuttersGrid.propTypes = {
